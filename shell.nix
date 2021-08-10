22edb9fc3c60b467e856896137c22aa6f7d9247d
sehqlr <hey@samhatfield.me>
2021-08-09 21:21:38 -0500

    Add niv. Add neuron and vale to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  sources = import ./nix/sources.nix;
+  neuron = (import sources.neuron).default;
+in { pkgs ? import sources.nixpkgs { } }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.epr
-    pkgs.tectonic
-  ];
-}
+pkgs.mkShell { buildInputs = [ pkgs.vale neuron ]; }
